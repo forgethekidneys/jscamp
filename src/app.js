7afec493364639b89c31ca06746a97c1b191488a
ahmet eren akbaş <akbaskerim82@hotmail.com>
2021-12-27 23:42:54 +0300

    27 aralık 23.42

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("Maraba yine bne")
+
+let dolarDun = 9.20
+dolarDun ="Ankara"
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+//sabit
+
+console.log(euroDun)
+
+
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+
+console.log("ul")
+
+for(let i = 0 ; i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"<li>")
+}
+
+console.log("ul")
